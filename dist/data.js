0c6ba2759cc23bc3f749256c05bf2c040e98b995
rnguyen89 <rnguyen89@gmail.com>
2022-08-20 00:13:50 -0700

    added test data

--- dist/data.js
+++ dist/data.js
@@ -1,1 +1,1 @@
-console.log("hello from data");
+console.log("hello from data");const DATA=[{city:"New York",content:"test content",image:"this will be an url"},{city:"Los Angeles",content:"test content",image:"this will be an url"},{city:"Tokyo",content:"test content",image:"this will be an url"}];console.log(DATA);
