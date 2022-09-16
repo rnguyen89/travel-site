c1f7de4aa7e5c5e1122fd6ea85f180d94b6a1dde
rnguyen89 <rnguyen89@gmail.com>
2022-09-15 23:12:49 -0700

    styled mobile nav

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,6 @@ const navMenuLinks = document.querySelector(".navMobileLinks");
 navMenu.addEventListener("click", function () {
   console.log("menu clicked");
   navMenuLinks.classList.toggle("hide");
-  element.classList.toggle("mystyle");
 });
 
 let hiddenTags = document.querySelectorAll(".hidden");
@@ -24,3 +23,68 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenTags.forEach((el) => observer.observe(el));
+
+// destinations
+
+const numSteps = 20.0;
+
+let boxElement;
+let prevRatio = 0.0;
+let increasingColor = "rgba(40, 40, 190, ratio)";
+let decreasingColor = "rgba(190, 40, 40, ratio)";
+
+// Set things up
+window.addEventListener(
+  "load",
+  (event) => {
+    boxElement = document.querySelector("#box");
+
+    createObserver();
+  },
+  false
+);
+
+function createObserver() {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: buildThresholdList(),
+  };
+
+  observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(boxElement);
+}
+
+function buildThresholdList() {
+  let thresholds = [];
+  let numSteps = 20;
+
+  for (let i = 1.0; i <= numSteps; i++) {
+    let ratio = i / numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  console.log(thresholds);
+  return thresholds;
+}
+
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > prevRatio) {
+      entry.target.style.backgroundColor = increasingColor.replace(
+        "ratio",
+        entry.intersectionRatio
+      );
+    } else {
+      entry.target.style.backgroundColor = decreasingColor.replace(
+        "ratio",
+        entry.intersectionRatio
+      );
+    }
+
+    prevRatio = entry.intersectionRatio;
+  });
+}
